8357b03a4da30985b90b318abd47441a96836711
bartek-k <bartek-k@localhost>
2015-06-03 15:19:45 +0300

    fixed dnsmasq adblock

--- freebsd/usr/local/sbin/update-adblock.sh
+++ freebsd/usr/local/sbin/update-adblock.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-WIN2002_LIST=winhelp2002.mvps.org.list
+WIN2002_LIST=winhelp2002.mvps.org.block
 WINHELP_LIST_OUT=/tmp/$WIN2002_LIST
+DEST="/usr/local/etc/dnsmasq.d/$WIN2002_LIST"
+
 
 echo "Proccessing: $WIN2002_LIST"
 wget http://winhelp2002.mvps.org/hosts.txt -O $WINHELP_LIST_OUT -q
-egrep '^0\.0\.0\.0' /tmp/winhelp2002.mvps.org.list | sed -e 's/\r//g' | awk '{ printf "address=/.%s/127.0.0.1\n", $2 } ' > /home/$WIN2002_LIST
-echo "Total: " `wc -l /home/$WIN2002_LIST`
+egrep '^0\.0\.0\.0' $WINHELP_LIST_OUT | sed -e 's/\r//g' | awk '{ printf "address=/.%s/127.0.0.1\n", $2 } ' > $DEST
+echo "Total: " `wc -l $DEST`
 
 echo "Restarting dnsmasq"
 service dnsmasq restart
